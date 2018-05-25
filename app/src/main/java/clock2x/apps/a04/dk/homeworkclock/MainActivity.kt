cd31c67a495307d49256ed325f5de9aebbb426ce
Piotr Czarny <piotr@mobilepeople.com>
2018-05-25 14:54:29 +0200

    Adding files from Android Studio project

---
+++ app/src/main/java/clock2x/apps/a04/dk/homeworkclock/MainActivity.kt
@@ -0,0 +1,12 @@
+package clock2x.apps.a04.dk.homeworkclock
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
