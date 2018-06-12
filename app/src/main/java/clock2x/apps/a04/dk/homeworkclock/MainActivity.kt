e27cee0f4b15b3c5fd7bfa7dee914b9eb44a888c
Piotr Czarny <piotr@mobilepeople.com>
2018-06-12 10:51:09 +0200

    Added notification Removed findViewById. Doing it Kotlin style now.

--- app/src/main/java/clock2x/apps/a04/dk/homeworkclock/MainActivity.kt
+++ app/src/main/java/clock2x/apps/a04/dk/homeworkclock/MainActivity.kt
@@ -2,30 +2,37 @@ package clock2x.apps.a04.dk.homeworkclock
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.NotificationCompat
 import android.support.v7.app.AlertDialog
+import android.util.Log
 import android.view.animation.Animation
 import android.view.animation.RotateAnimation
-import android.widget.ImageButton
-import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
+import android.app.NotificationManager
+import android.app.NotificationChannel
+import android.os.Build
+import android.support.v4.app.NotificationManagerCompat
+import android.app.PendingIntent
+import android.content.Intent
+
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var timer1 : TimerView
-    private lateinit var timer2 : TimerView
-    private lateinit var arrowView: ImageView
 
-    private lateinit var resetButton : ImageButton
+    private var crCount = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        crCount++
         setContentView(R.layout.activity_main)
-
-        timer1 = findViewById(R.id.timer1)
-        timer2 = findViewById(R.id.timer2)
-        arrowView = findViewById(R.id.arrow)
-
-        resetButton = findViewById(R.id.resetButton)
+        createNotificationChannel()
 
         timer1.setOnClickListener({ v -> run {
             if(timer1.isRunning) {
@@ -72,6 +79,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        clearNotification()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if(timer1.isRunning || timer2.isRunning) {
+            showNotification()
+        }
     }
 
     private var prevDeg : Float = 0f;
@@ -98,7 +113,47 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        arrowView.startAnimation(anim)
+        arrow.startAnimation(anim)
+    }
 
+    val CHANNEL_ID = "homeworkclockchannel"
+
+    private fun showNotification() {
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.abc_ic_arrow_drop_right_black_24dp)
+                .setContentTitle("Title")
+                .setContentText("Some text")
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        val noget = builder.build()
+
+        val notificationManager = NotificationManagerCompat.from(this)
+        notificationManager.notify(111, noget)
+    }
+
+    private fun clearNotification() {
+        val notificationManager = NotificationManagerCompat.from(this)
+        notificationManager.cancel(111)
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val description = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = description
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager!!.createNotificationChannel(channel)
+        }
     }
 }
